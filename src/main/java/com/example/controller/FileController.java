23fd8c26740a0c79bed3f0f3cc071e14159878cf
Excalibur0w0 <752408303@qq.com>
2020-03-27 14:59:36 +0800

    upload file

---
+++ src/main/java/com/example/controller/FileController.java
@@ -0,0 +1,17 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ * @author dqy
+ * @version 1.0
+ * @date 2020/3/27 下午2:58
+ */
+@Controller
+public class FileController {
+    @PostMapping
+    public void uploadFile() {
+
+    }
+}
